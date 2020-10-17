ebe6820d8c1d2f78b8c600a61f6085f5319b124c
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-10-16 23:52:14 -0300

    starting sword soul item

--- src/main/java/com/aferrercrafter/soulharvest/item/Soul.java
+++ src/main/java/com/aferrercrafter/soulharvest/item/Soul.java
@@ -1,4 +1,5 @@
 package com.aferrercrafter.soulharvest.item;
 
 public class Soul {
+
 }
---
+++ src/main/java/com/aferrercrafter/soulharvest/item/tool/SoulSword.java
@@ -0,0 +1,11 @@
+package com.aferrercrafter.soulharvest.item.tool;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.SwordItem;
+
+public class SoulSword extends SwordItem {
+
+    public SoulSword(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties p_i48460_4_) {
+        super(tier, attackDamageIn, attackSpeedIn, p_i48460_4_);
+    }
+}
